b1e6ef434e827e350fa3068d03f34cadcdf4617d
VeljkoRikalovic <velja001@yahoo.com>
2023-10-28 14:29:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,159 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((items) => items.id !== id));
+  }
+
+  function handlePacked(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirm = window.confirm(
+      "Are you sure you want to delete all items in your list?"
+    );
+    if (confirm) setItems([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        onClear={handleClearList}
+        items={items}
+        onDelete={handleDeleteItems}
+        onPacked={handlePacked}
+      />
+      <Stats items={items} />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴Far away💼</h1>;
+}
+
+function Form(props) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    props.onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList({ items, onDelete, onPacked, onClear }) {
+  const [sortBy, setSortby] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            onDelete={onDelete}
+            key={item.id}
+            onPacked={onPacked}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortby(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={(e) => onClear([])}>Clear list</button>
+      </div>
+    </div>
+  );
+}
+
+function Item({ item, onDelete, onPacked }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onPacked(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDelete(item.id)}>❌</button>
+    </li>
+  );
+}
+
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items to your packing list</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const procenat = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {procenat === 100
+          ? "You got everything! Ready to go✈️"
+          : `You have ${numItems} items on your list, and you already packed ${numPacked} ${procenat}`}
+      </em>
+    </footer>
+  );
+}
